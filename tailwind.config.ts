9244a9ec5be696ae072dadddb7af56c92cecf75b
Atle Kristiansen <atlekri@hotmail.com>
2024-10-18 07:34:03 +0200

    Initial commitInitial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sanity/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -18,6 +19,8 @@ const config: Config = {
       },
     },
   },
+  //@ts-ignore
+  theme: {},
   plugins: [],
 };
 export default config;
